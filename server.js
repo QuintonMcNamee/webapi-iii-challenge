fe16db0f71c2a4cf2b66296ac5a2ad2f42a38d39
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-09-08 15:31:34 -0400

    MVP reached

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require('express');
+
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
 
+// gloabl middleware
+server.use(express.json());
+server.use(logger); // custom middleware used globally
+
+// local middleware
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
+
+//custom middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request made to ${req.url})}`
+  );
+
+  next();
+};
+
+// route handlers
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
-
+// export default server
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,46 +1,144 @@
-const express = 'express';
+const express = require('express');
+
+const userDb = require('./userDb.js');
+
+const postDb = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
+  const test = req.body;
 
+  userDb.insert(test)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const id = req.params.id;
+  let post = req.body;
 
+  postDb.insert(post)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 router.get('/', (req, res) => {
-
+  userDb.get()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  const id = req.params.id;
 
+  userDb.getUserPosts(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const id = req.params.id;
 
+  userDb.getUserPosts(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
+  const id = req.params.id;
 
+  userDb.remove(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
+  const id = req.params.id;
+  const test = req.body;
 
+  userDb.update(id, test)
+    .then(response => {
+      res.status(200).json(update);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  const id = req.params.id;
 
+  userDb.getById(id)
+    .then(response => {
+      if(!response) {
+        res.status(404).json({ message: "Error" });
+      }
+      else {
+        req.user = response;
+        next();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
 
 function validateUser(req, res, next) {
+  const test1 = req.body.name;
+  const test2 = req.body;
+
+  if(!test1) {
+    res.status(404).json({ message: "Error" });
+  }
+
+  if(!test2) {
+    res.status(404).json({ message: "Error" });
+  }
+
+  next();
 
 };
 
 function validatePost(req, res, next) {
+  const test = req.body.text;
+  const test2 = Object.keys(req.body);
+
+  if(!test) {
+    res.status(404).json({ message: "Error" });
+  }
+
+  if(test2 === 0) {
+    res.status(404).json({ message: "Error" });
+  }
+
+  next();
 
 };
 
