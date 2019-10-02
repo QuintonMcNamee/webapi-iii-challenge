996e2813614960166dd55a8f713b034996538dcd
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-02 18:30:49 -0400

    added more custom middleware

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = express();
 // gloabl middleware
 server.use(express.json());
 server.use(logger); // custom middleware used globally
+server.use(locked);
 
 // local middleware
 server.use('/posts', postRouter);
@@ -22,10 +23,25 @@ function logger(req, res, next) {
   next();
 };
 
+function locked(req, res, next) {
+  if(req.url === '/locked') {
+    console.log('You may enter.');
+    res.send('You may enter.');
+    next();
+  } else {
+    console.log('You are locked outside the door.');
+    res.send('You are locked outside the door.');
+  }
+};
+
 // route handlers
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+server.get('/locked', locked, (req, res) => {
+  res.send()
+})
 
 // export default server
 module.exports = server;
